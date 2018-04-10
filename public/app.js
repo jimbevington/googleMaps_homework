1ed2b8b33a76101d57d8bd803345ef0538fa13ac
Jim Bevington <jim.bevington@hotmail.com>
2018-04-10 16:27:00 +0100

    added mapWrapper.js with constructor

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ const initalize = function(){
 
   const container = document.getElementById('map-div');
   const startCenter = {lat: 0, lng: 0};
-  const startZoom = 1;
+  const startZoom = 2;
 
   const map = new MapWrapper(container, startCenter, startZoom);
 
