e6673ada804b080d7bb6f2dd84c28ec0e74f5dee
Jim Bevington <jim.bevington@hotmail.com>
2018-04-10 16:18:02 +0100

    added startCenter, startZoom params, instantiated map object

--- public/app.js
+++ public/app.js
@@ -1,6 +1,10 @@
 const initalize = function(){
 
   const container = document.getElementById('map-div');
+  const startCenter = {lat: 0, lng: 0};
+  const startZoom = 1;
+
+  const map = new MapWrapper(container, startCenter, startZoom);
 
 }
 
